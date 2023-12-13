b95a950c18c3c505e552181fd42ca9745816623b
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-12-13 14:10:50 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,31 @@ const App = () => {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
 
-  const handleOnSUbmit = async(e) =>{
-    
+  const handleOnSubmit = async(e) =>{
+    e.preventDefault()
+    let result = await fetch('https://localhost:5000/',{
+      method: 'POST',
+      body: JSON.stringify({name,email}),
+      headers: {'Content-Type': 'application/json'}
+    })
+    result = await result.json()
+    console.log(result)
+    if(result){
+      alert('Data sent successfully')
+      setEmail("")
+      setName("")
+    }
+
   }
 
   return (
     <div>
-      <h1>
+      <h1>This is React WebApp</h1>
         <form action="">
-          <input type="text" name="" id="" />
+          <input type="text" placeholder="name=" value={name} onChange={(e) => setName(e.target.value)}/>
+          <input type="email" placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <button type="submit" onClick={handleOnSubmit}>Submit</button>
           </form>
-      </h1>
     </div>
   )
 }
